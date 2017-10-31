276b58daf814bafcdd8b89b9c1676b7611362661
taylor.wilkerson <p75r335@bz-cs-cslab17l.msu.montana.edu>
2017-10-30 18:41:50 -0600

    Got part 3 to work!

---
+++ network_3.py
@@ -0,0 +1,228 @@
+'''
+Created on Oct 12, 2016
+
+@author: mwitt_000
+'''
+import queue
+import math
+import threading
+
+class TableEntry:
+    def __init__(self, to_addr, from_addr, outport):
+        self.to_addr = to_addr
+        self.from_addr = from_addr
+        self.outport = outport
+
+## wrapper class for a queue of packets
+class Interface:
+    ## @param maxsize - the maximum size of the queue storing packets
+    def __init__(self, maxsize=0):
+        self.queue = queue.Queue(maxsize);
+        self.mtu = None
+    
+    ##get packet from the queue interface
+    def get(self):
+        try:
+            return self.queue.get(False)
+        except queue.Empty:
+            return None
+        
+    ##put the packet into the interface queue
+    # @param pkt - Packet to be inserted into the queue
+    # @param block - if True, block until room in queue, if False may throw queue.Full exception
+    def put(self, pkt, block=False):
+        self.queue.put(pkt, block)
+
+
+## Implements a network layer packet (different from the RDT packet 
+# from programming assignment 2).
+# NOTE: This class will need to be extended to for the packet to include
+# the fields necessary for the completion of this assignment.
+class NetworkPacket:
+    ## packet encoding lengths 
+    dst_addr_S_length = 5
+    header_length = dst_addr_S_length * 3 + 1
+    pacs_sent = 0
+    
+    ##@param dst_addr: address of the destination host
+    # @param data_S: packet payload
+    def __init__(self, dst_addr, source_addr, data_S, segNum, moreflag=0):
+        self.dst_addr = dst_addr
+        self.source_addr = source_addr
+        self.data_S = data_S
+        self.size = len(data_S) + (self.dst_addr_S_length * 3) + 1
+        self.segNum = segNum
+        self.moreflag = moreflag
+
+        
+    ## called when printing the object
+    def __str__(self):
+        return self.to_byte_S() 
+
+    def split(self, size):
+        
+        new_packets = []
+        seg_num = self.segNum
+        
+        while len(self.data_S) > 0:
+            if len(self.data_S) < size:
+                p = NetworkPacket(self.dst_addr,self.source_addr, self.data_S, seg_num, self.moreflag)
+                new_packets.append(p)
+                self.data_S = ''
+            else:
+                temp1 = self.data_S[:size ]
+                p = NetworkPacket(self.dst_addr, self.source_addr, temp1, seg_num, 1)
+                new_packets.append(p)
+                self.data_S = self.data_S[size:] #shorten data_S
+        
+        return new_packets
+    
+    ## convert packet to a byte string for transmission over links
+    def to_byte_S(self):
+        byte_S = str(self.dst_addr).zfill(self.dst_addr_S_length)
+        byte_S += str(self.source_addr).zfill(self.dst_addr_S_length)
+        byte_S += str(self.segNum).zfill(self.dst_addr_S_length)
+        byte_S += str(self.moreflag)
+        byte_S += self.data_S
+        return byte_S
+    
+    ## extract a packet object from a byte string
+    # @param byte_S: byte string representation of the packet
+    @classmethod
+    def from_byte_S(self, byte_S):
+        dst_addr = int(byte_S[0 : NetworkPacket.dst_addr_S_length])
+        source_addr = int(byte_S[ NetworkPacket.dst_addr_S_length:(NetworkPacket.dst_addr_S_length*2)])
+        segNum = int(byte_S[NetworkPacket.dst_addr_S_length*2:(NetworkPacket.dst_addr_S_length*3)])
+        moreflag = int(byte_S[(NetworkPacket.dst_addr_S_length * 3):((NetworkPacket.dst_addr_S_length*3)+1)])
+        data_S = byte_S[((NetworkPacket.dst_addr_S_length*3)+1): ]
+        # print("Destination: %s " % dst_addr)
+        # print("Seg num: %s" % segNum)
+        # print("moreflag: %s" % moreflag)
+        return self(dst_addr, source_addr, data_S, segNum, moreflag)
+    
+
+    
+
+## Implements a network host for receiving and transmitting data
+class Host:
+    idNum = 0
+    ##@param addr: address of this node represented as an integer
+    def __init__(self, addr):
+        self.addr = addr
+        self.in_intf_L = [Interface()]
+        self.out_intf_L = [Interface()]
+        self.stop = False #for thread termination
+    
+    ## called when printing the object
+    def __str__(self):
+        return 'Host_%s' % (self.addr)
+       
+    ## create a packet and enqueue for transmission
+    # @param dst_addr: destination address for the packet
+    # @param data_S: data being transmitted to the network layer
+    def udt_send(self, dst_addr, data_S):
+        p = NetworkPacket(dst_addr,self.addr, data_S, Host.idNum)
+        Host.idNum = Host.idNum + 1
+        new_packets = p.split(self.out_intf_L[0].mtu - NetworkPacket.header_length)
+        for pac in new_packets:
+            self.out_intf_L[0].put(pac.to_byte_S()) #send packets always enqueued successfully
+            print('%s: sending packet "%s" out interface with mtu=%d' % (self, pac, self.out_intf_L[0].mtu))
+
+        
+            
+    # def udt_receive(self):
+    #     pkt_S = self.in_intf_L[0].get()
+    #     if pkt_S is not None:
+    #         print('%s: received packet "%s"' % (self, pkt_S))
+
+            
+    ## receive packet from the network layer
+    def udt_receive(self):
+        #while loop until whole message is received
+        pkt_S = self.in_intf_L[0].get()
+        received_pacs = ''
+        if pkt_S is not None:
+            pac = NetworkPacket.from_byte_S(pkt_S)
+            while pac.moreflag == 1:
+                if pkt_S is not None:
+                    pac = NetworkPacket.from_byte_S(pkt_S)
+                    received_pacs = received_pacs + pac.data_S
+                pkt_S = self.in_intf_L[0].get()
+
+            # packets are already joined, print the whole one out
+            print("\n")
+            print('%s: recieved whole packet "%s"' % (self, received_pacs))
+            print("\n")
+
+            
+    ## thread target for the host to keep receiving data
+    def run(self):
+        print (threading.currentThread().getName() + ': Starting')
+        while True:
+            #receive data arriving to the in interface
+            self.udt_receive()
+            #terminate
+            if(self.stop):
+                print (threading.currentThread().getName() + ': Ending')
+                return
+        
+
+
+## Implements a multi-interface router described in class
+class Router:
+    
+    ##@param name: friendly router name for debugging
+    # @param intf_count: the number of input and output interfaces 
+    # @param max_queue_size: max queue length (passed to Interface)
+    def __init__(self, name, intf_count, max_queue_size, r_table):
+        self.r_table = r_table
+        self.stop = False #for thread termination
+        self.name = name
+        #create a list of interfaces
+        self.in_intf_L = [Interface(max_queue_size) for _ in range(intf_count)]
+        self.out_intf_L = [Interface(max_queue_size) for _ in range(intf_count)]
+
+    ## called when printing the object
+    def __str__(self):
+        return 'Router_%s' % (self.name)
+
+
+    def determine_interface(self, packet):
+        for entry in self.r_table:
+            if entry.to_addr == packet.dst_addr and packet.source_addr == entry.from_addr:
+                return entry.outport
+        raise "Table entry not pressent. Stuff should explode..."
+    
+    ## look through the content of incoming interfaces and forward to
+    # appropriate outgoing interfaces
+    def forward(self):
+        for i in range(len(self.in_intf_L)):
+            pkt_S = None
+            try:
+                #get packet from interface i
+                pkt_S = self.in_intf_L[i].get()
+                #if packet exists make a forwarding decision
+                if pkt_S is not None:
+                    p = NetworkPacket.from_byte_S(pkt_S) #parse a packet out
+                    # HERE you will need to implement a lookup into the 
+                    # forwarding table to find the appropriate outgoing interface
+                    # for now we assume the outgoing interface is also i
+                    # split packets
+                    out_inter = self.determine_interface(p)
+                    new_packets = p.split(self.out_intf_L[out_inter].mtu - NetworkPacket.header_length)
+                    for pac in new_packets:
+                        self.out_intf_L[out_inter].put(pac.to_byte_S()) #send packets always enqueued successfully
+                        print('%s: sending packet "%s" out interface with mtu=%d' % (self, pac, self.out_intf_L[out_inter].mtu))
+            except queue.Full:
+                print('%s: packet "%s" lost on interface %d' % (self, p, i))
+                pass
+            
+                
+    ## thread target for the host to keep forwarding data
+    def run(self):
+        print (threading.currentThread().getName() + ': Starting')
+        while True:
+            self.forward()
+            if self.stop:
+                print (threading.currentThread().getName() + ': Ending')
+                return 
---
+++ simulation_3.py
@@ -0,0 +1,110 @@
+'''
+Created on Oct 12, 2016
+
+@author: mwitt_000
+'''
+import network
+import link
+import threading
+from time import sleep
+
+##configuration parameters
+router_queue_size = 0 #0 means unlimited
+simulation_time = 40 #give the network sufficient time to transfer all packets before quitting
+
+if __name__ == '__main__':
+    object_L = [] #keeps track of objects, so we can kill their threads
+    
+    #create network nodes
+    client1 = network.Host(1)
+    client2 = network.Host(2)
+    
+    object_L.append(client1)
+    object_L.append(client2)
+    
+    server1 = network.Host(3)
+    server2 = network.Host(4)
+    object_L.append(server1)
+    object_L.append(server2)
+
+
+    tableA = [network.TableEntry(3, 1, 0), network.TableEntry(4,1,0), network.TableEntry(3, 2, 1), network.TableEntry(4,2,1)]
+    tableB = [network.TableEntry(3, 1, 0), network.TableEntry(4, 1, 0)]
+    tableC = [network.TableEntry(3, 2, 0), network.TableEntry(4, 2, 0)]
+    tableD = [network.TableEntry(3, 1, 0), network.TableEntry(4,1,1), network.TableEntry(3, 2, 0), network.TableEntry(4,2,1)]
+
+    
+    router_a = network.Router(name='A', intf_count=2, max_queue_size=router_queue_size, r_table=tableA)
+    router_b = network.Router(name='B', intf_count=1, max_queue_size=router_queue_size, r_table=tableB)
+    router_c = network.Router(name='C', intf_count=1, max_queue_size=router_queue_size, r_table=tableC)
+    router_d = network.Router(name='D', intf_count=2, max_queue_size=router_queue_size, r_table=tableD)
+    
+    object_L.append(router_a)
+    object_L.append(router_b)
+    object_L.append(router_c)
+    object_L.append(router_d)
+    
+    #create a Link Layer to keep track of links between network nodes
+    link_layer = link.LinkLayer()
+    object_L.append(link_layer)
+    
+    #add all the links
+    link_layer.add_link(link.Link(client1, 0, router_a, 0, 50))
+    link_layer.add_link(link.Link(client2, 0, router_a, 1, 50))
+    
+    link_layer.add_link(link.Link(router_a, 0, router_b, 0, 30))
+    link_layer.add_link(link.Link(router_a, 1, router_c, 0, 30))
+    
+    link_layer.add_link(link.Link(router_b, 0, router_d, 0, 30))
+    link_layer.add_link(link.Link(router_c, 0, router_d, 1, 30))
+    
+    link_layer.add_link(link.Link(router_d, 0, server1, 0, 30))
+    link_layer.add_link(link.Link(router_d, 1, server2, 0, 30))
+
+    
+    
+    #start all the objects
+    thread_L = []
+    thread_L.append(threading.Thread(name=client1.__str__(), target=client1.run))
+    thread_L.append(threading.Thread(name=client2.__str__(), target=client2.run))
+    thread_L.append(threading.Thread(name=server1.__str__(), target=server1.run))
+    thread_L.append(threading.Thread(name=server2.__str__(), target=server2.run))
+
+    
+    thread_L.append(threading.Thread(name=router_a.__str__(), target=router_a.run))
+    thread_L.append(threading.Thread(name=router_b.__str__(), target=router_b.run))
+    thread_L.append(threading.Thread(name=router_c.__str__(), target=router_c.run))
+    thread_L.append(threading.Thread(name=router_d.__str__(), target=router_d.run))
+    
+    thread_L.append(threading.Thread(name="Network", target=link_layer.run))
+    
+    for t in thread_L:
+        t.start()
+    
+    
+    # #create some send events    
+    for i in range(2):
+        #client.udt_send(2, 'Sample data %d.' % i)
+        # client1.udt_send(3, 'Sample data that is really, really, incredibly interesting %d. Its so interesting, it has two sentences!' % i)
+        client1.udt_send(4, 'Sample data that is really, really, incredibly interesting %d. Its so interesting, it has two sentences!' % i)
+        
+    # for i in range(1):
+    #     #client.udt_send(2, 'Sample data %d.' % i)
+    #     client2.udt_send(3, 'Sample data that is really, really, incredibly interesting %d. Its so interesting, it has two sentences!' % i)
+    #     # client2.udt_send(4, 'Sample data that is really, really, incredibly interesting %d. Its so interesting, it has two sentences!' % i)
+    
+    
+    #give the network sufficient time to transfer all packets before quitting
+    sleep(simulation_time)
+    
+    #join all threads
+    for o in object_L:
+        o.stop = True
+    for t in thread_L:
+        t.join()
+        
+    print("All simulation threads joined")
+
+
+
+# writes to host periodically
